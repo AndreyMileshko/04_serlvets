aa43e2088ecc198fea647bc127b4059a3cde3d2b
Andrey Mileshko <AndreyPrograming.Mileshko@yandex.ru>
2024-10-06 00:56:58 +0700

    replace list on map

--- servlets/src/main/java/ru/netology/servlet/handler/Handler.java
+++ servlets/src/main/java/ru/netology/servlet/handler/Handler.java
@@ -1,6 +1,7 @@
 package ru.netology.servlet.handler;
 
 import ru.netology.controller.PostController;
+import ru.netology.exception.NotFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,19 +9,18 @@ import javax.servlet.http.HttpServletResponse;
 import static ru.netology.servlet.handler.Method.*;
 
 public class Handler {
-    public void serviceHandler(HttpServletRequest req, HttpServletResponse resp, PostController controller) {
+    public static void serviceHandler(HttpServletRequest req,
+                                      HttpServletResponse resp,
+                                      PostController controller) {
         try {
             final var path = req.getRequestURI();
             final var method = req.getMethod();
-            // primitive routing
             if (method.equals(GET.getMethod()) && path.equals("/api/posts")) {
                 controller.all(resp);
                 return;
             }
             if (method.equals(GET.getMethod()) && path.matches("/api/posts/\\d+")) {
-                // easy way
-                final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
-                System.out.println(id);
+                final var id = parseID(path);
                 controller.getById(id, resp);
                 return;
             }
@@ -29,16 +29,23 @@ public class Handler {
                 return;
             }
             if (method.equals(DELETE.getMethod()) && path.matches("/api/posts/\\d+")) {
-                // easy way
-                final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+                final var id = parseID(path);
                 controller.removeById(id, resp);
                 return;
             }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+        } catch (NotFoundException e) {
+            e.printStackTrace();
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
         } catch (Exception e) {
             e.printStackTrace();
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private static long parseID(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
     }
 }
 
